572186e281afa950689be40e19d452a665c705c0
tanya <jjg-lessons@gmail.com>
2024-02-25 01:56:23 +0300

    Отредактирован файл HomeWork04

--- src/com/tatiana/project/lesson6/Lesson06.java
+++ src/com/tatiana/project/lesson6/Lesson06.java
@@ -17,11 +17,11 @@ public class Lesson06 {
         Author author03 = new Author("Макар");
         Author author04 = new Author(3, "Саша", "sasha@gmail.com");
 
-        Book book01 = new Book(1, "JDK21", 100);
-        book01.setNumberOfPages(132);
-        book01.setPrice(200);
+        // ошибка в этой строке   Book book01 = new Book(1, "JDK21", 100);
+        //book01.setNumberOfPages(132);
+        //book01.setPrice(200);
 
-        System.out.println(book01.getId());
+        //System.out.println(book01.getId());
 
 
 
